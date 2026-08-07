4ea833fbcc22c955d68c5abe54696d29d74d515f
Juan Ramon Valencia <jvalencia658@gmail.com>
2026-08-07 21:33:38 +0200

    add type alias for preprocessing steps

--- src/data/preprocessing/PreprocessingPipeline.py
+++ src/data/preprocessing/PreprocessingPipeline.py
@@ -1,18 +1,22 @@
 
-import tensorflow as tf
+from typing import TypeAlias
 from omegaconf import DictConfig
+from collections.abc import Callable
 
+import tensorflow as tf
+
+PreprocessingStep: TypeAlias = Callable[[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]
 
 class PreprocessingPipeline:
     """Apply the preprocessing steps configured in the dataset configuration."""
 
     def __init__(self, cfg: DictConfig) -> None:
         self._cfg = cfg
-        self._steps_registry = {
-            "pixel-value-normalization": self._normalization_fn
-            }
+        self._steps_registry: dict[str, PreprocessingStep] = {
+            "pixel-value-normalization": self._pixel_value_normalization
+        }
 
-    def _resolve_step(self, step_name: str):
+    def _resolve_step(self, step_name: str) -> PreprocessingStep:
         """Resolve a preprocessing step name to its corresponding function"""
 
         try:
@@ -20,7 +24,7 @@ class PreprocessingPipeline:
         except KeyError as e:
             raise ValueError(f"Unknown preprocessing step: {step_name}") from e
         
-    def _normalization_fn(self, image: tf.Tensor, label: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
+    def _pixel_value_normalization(self, image: tf.Tensor, label: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
         """Normalize image pixel values by scaling them from [0, 255] to [0, 1]."""
 
         image = tf.cast(image/255.0, tf.float32)
@@ -29,10 +33,10 @@ class PreprocessingPipeline:
     def apply(self, dataset: tf.data.Dataset) -> tf.data.Dataset:
         """Apply the configured preprocessing pipeline to a dataset."""
 
-        process_fns = [
+        process_fns: list[PreprocessingStep] = [
             self._resolve_step(step_name) 
             for step_name in self._cfg.dataset.preprocessing.steps
-            ]
+        ]
         
         if not process_fns:
             return dataset
