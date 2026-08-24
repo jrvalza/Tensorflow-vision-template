9884d5d104ffbc7391fead9833b8af47019fab00
Juan Ramon Valencia <jvalencia658@gmail.com>
2026-08-25 01:04:15 +0200

    The files check_dependencies have been added

---
+++ scripts/check_dependencies.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import sklearn
+import numpy as np
+import matplotlib
+import hydra
+import omegaconf
+
+print('Tensorflow version: ', tf.__version__)
+print('GPUS: ', tf.config.list_physical_devices('GPU'))
+print('GPUS: ', tf.config.list_physical_devices('GPU').__len__() > 0)
+
+print('Scikit-learn version: ', sklearn.__version__)
+print('Numpy version: ', np.__version__)
+print('matplotlib version: ', matplotlib.__version__)
+print('Hydra version: ', hydra.__version__)
+print('OmegaConf version: ', omegaconf.__version__)
